1b2f6687f2638da22c540418bd0325f41db4cf79
MehBirb <john.lu10212004@gmail.com>
2022-06-30 01:52:43 -0400

    Learned how to utilize components

--- src/index.js
+++ src/index.js
@@ -2,16 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Foo = ({name}) =>{
+  return(
+    <p>This is an item component. Its name is {name}</p>
+  )
+}
+
+const Item = (props) => {
+  return(
+    <div> 
+      <p>This is an item component. Its name is {props.name}</p>
+      <Foo name = {'UwU'} />
+      <Foo name = {'OwO'} />
+      {item}
+    </div>
+  )
+}
+
+const item = <p>This is an item</p>
+
+const element= <div>
+  <h1>Hello, People</h1>
+  <p>This is a page test</p>
+  <Item name = {'-w-'}/>
+</div>
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    //<App />,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
